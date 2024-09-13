0faa5a821b3bc7492cdcc503dbfe3f37840f46be
Rafi1072 <rafi110-96@o2.pl>
2024-09-13 10:30:06 +0200

    task-3-done

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,12 @@
 const textInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
-//const anon = textInput.trim();
 
 textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value;
-  // if (textInput === "" || anon) {
-  // return output.textContent("Anonymous");
-  // }
+  const inputValue = event.currentTarget.value.trim();
+
+  if (inputValue === "") {
+    output.textContent = "Anonymous";
+  } else {
+    output.textContent = inputValue;
+  }
 });
