7cae66fae5855f19230e49244f8294d0d8e3988b
Rafi1072 <rafi110-96@o2.pl>
2024-09-13 11:57:47 +0200

    task-4-worked

--- js/task-4.js
+++ js/task-4.js
@@ -2,4 +2,30 @@ const form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  const inputEmail = document.querySelector(`input[type="email"]`);
+  const inputPassword = document.querySelector(`input[type="password"]`);
+
+  if (inputEmail.value === "" || inputPassword.value === "") {
+    return alert("All form fields must be filled in");
+  }
 });
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formLogin = event.target;
+  const email = formLogin.elements.email.value;
+  const password = formLogin.elements.password.value;
+
+  console.log(`Email: ${email}, Password: ${password}`);
+  form.reset();
+
+  const data = {
+    email,
+    password,
+  };
+
+  handleSubmit.push(data);
+  console.log(data);
+}
